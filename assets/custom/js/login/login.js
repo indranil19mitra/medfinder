b8a5d011be7bbb5d8bd70051ed80379917e91b9d
Indranil Mitra <110732658+indranil19mitra@users.noreply.github.com>
2025-03-23 18:23:15 +0530

    helper function initialization

--- assets/custom/js/login/login.js
+++ assets/custom/js/login/login.js
@@ -1,8 +1,16 @@
 function login() {
-	// alert("login");
+	alert("login");
 	const formData = new FormData($("#loginForm")[0]);
 	$.ajax({
 		type: "post",
 		url: baseurl + "auth/authLogin",
+		data: formData,
+		dataType: "json",
+		contentType: false,
+		processData: false,
+		catch: false,
+		success: function (res) {
+			console.log(res);
+		},
 	});
 }
